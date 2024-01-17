77662ced93080579ebf54f8b6f86ac83e00441ad
Wyn213 <156633596+Wyn213@users.noreply.github.com>
2024-01-16 22:21:55 -0800

    possiabley broken

---
+++ scripts/index.js
@@ -0,0 +1,39 @@
+const eduList = [
+    {
+        name: '1',
+        link: 'https://hgentry.github.io/1/',
+        img: 'https://main.dd8vt524eqsk2.amplifyapp.com/projects/1/meta/apple-touch-icon.png'
+    }
+]
+
+eduList.forEach(eduApp => {
+    var cont = document.createElement("a");
+    cont.classList.add("game-link container");
+
+    var div = document.createElement("div");
+    div.classList.add("game-tile");
+
+    var appimg = document.createElement("img");
+    appimg.src = eduApp.img;
+
+    var title = document.createElement("h1");
+    title.innerHTML = eduApp.title;
+    title.classList.add("game-title");
+
+    document.getElementById("flex-container").appendChild(cont);
+    cont.appendChild(div);
+    div.appendChild(appimg);
+    div.appendChild(title);
+});
+
+
+/* html Stuct
+
+    <a href="ok" class="game-link container">
+      <div class="game-tile">
+        <img class="game-icon" src="https://main.dd8vt524eqsk2.amplifyapp.com/projects/1/meta/apple-touch-icon.png" loading="lazy">
+        <h1 class="game-title">1</h1>
+      </div>
+    </a>
+
+*/
